482959bbde4a9447d3a95411c226f64305444db2
on1RB <dudkevich91@gmail.com>
2022-07-26 18:02:04 +0300

    выполнил проект

--- source/js/main.js
+++ source/js/main.js
@@ -10,6 +10,37 @@ window.addEventListener('DOMContentLoaded', () => {
 
   iosVhFix();
 
+  const navMain = document.querySelector('.main-nav');
+  const navToggle = document.querySelector('.page-header__toggle');
+  const navLogo = document.querySelector('.page-header__logo');
+  const headerTitle = document.querySelector('.page-header__title');
+  const headerDesc = document.querySelector('.page-header__description');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', () => {
+    if (navToggle.classList.contains('page-header__toggle--closed')) {
+      navToggle.classList.remove('page-header__toggle--closed');
+      navToggle.classList.add('page-header__toggle--opened');
+      navLogo.classList.remove('page-header__logo--active');
+    } else {
+      navToggle.classList.add('page-header__toggle--closed');
+      navToggle.classList.remove('page-header__toggle--opened');
+      headerTitle.classList.add('page-header__title--notvisible');
+      headerDesc.classList.add('page-header__description--notvisible');
+    }
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+      navLogo.classList.add('page-header__logo--active');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+      headerTitle.classList.remove('page-header__title--notvisible');
+      headerDesc.classList.remove('page-header__description--notvisible');
+    }
+  });
+
   // Modules
   // ---------------------------------
 
@@ -19,7 +50,6 @@ window.addEventListener('DOMContentLoaded', () => {
     initModals();
   });
 });
-
 // ---------------------------------
 
 // ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
