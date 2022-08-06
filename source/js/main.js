be54a224101226ddfc1a31fd07e336ac5b892101
on1RB <dudkevich91@gmail.com>
2022-08-06 12:16:13 +0300

    поправил ошибки

--- source/js/main.js
+++ source/js/main.js
@@ -17,11 +17,14 @@ window.addEventListener('DOMContentLoaded', () => {
   const navLink = document.querySelectorAll('.main-nav__link');
   const navLinkList = Array.prototype.slice.call(navLink);
   const navOverley = document.querySelector('.main-nav__bg');
+  const navList = document.querySelector('.main-nav__list');
+  const textVisible = document.querySelector('.page-header__inner');
 
   navMain.classList.remove('main-nav--nojs');
   navMain.classList.add('main-nav--closed');
   navToggle.classList.remove('page-header__toggle--closed');
   navToggle.classList.add('page-header__toggle--opened');
+  navList.classList.remove('main-nav__list--nojs');
 
   navToggle.addEventListener('click', () => {
     if (navToggle.classList.contains('page-header__toggle--closed')) {
@@ -29,10 +32,17 @@ window.addEventListener('DOMContentLoaded', () => {
       navToggle.classList.add('page-header__toggle--opened');
       navLogo.classList.remove('page-header__logo--active');
       pageBody.classList.remove('scroll-lock-ios');
+      navOverley.classList.remove('main-nav__bg--active');
+      navList.classList.remove('main-nav__list--active');
+      textVisible.classList.remove('page-header__inner--invisible');
     } else {
       navToggle.classList.add('page-header__toggle--closed');
       navToggle.classList.remove('page-header__toggle--opened');
       pageBody.classList.add('scroll-lock-ios');
+      navOverley.classList.remove('main-nav__bg--active');
+      navOverley.classList.add('main-nav__bg--active');
+      navList.classList.add('main-nav__list--active');
+      textVisible.classList.add('page-header__inner--invisible');
     }
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
